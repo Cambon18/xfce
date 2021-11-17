695317b17dbca938fcf919df98d6838d6038b741
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-11-17 19:24:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 echo -e "\n>>Instalando XFCE\c"
 trizen --noconfirm -Sy xfce4-whiskermenu-plugin xfce4-session xfce4-panel xfce4-power-manager xfce4-settings  xfce4-screenshooter xfconf xfdesktop xfwm4 network-manager-applet networkmanager-openvpn xfce4-pulseaudio-plugin pulseaudio pulseaudio-jack pulseaudio-bluetooth pavucontrol menulibre mugshot
-cp -r xfce /etc/skel
+sudo cp -r xfce /etc/skel
