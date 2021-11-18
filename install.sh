9fb46dc7a59109183028d16ae3798c3a80bcd557
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-11-18 09:12:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo -e "\n>>Instalando XFCE\c"
 trizen --noconfirm -Sy xfce4-whiskermenu-plugin xfce4-session xfce4-panel xfce4-power-manager xfce4-settings  xfce4-screenshooter xfconf xfdesktop xfwm4 network-manager-applet xfce4-pulseaudio-plugin pulseaudio pulseaudio-jack pulseaudio-bluetooth pavucontrol menulibre mugshot
-trizen --noconfirm -Sy thunar xdg-user-dirs gvfs gvfs-smb thunar-volman thunar-archive-plugin file-roller gnome-disk-utility mousepad vlc atril ristretto galculator xfce4-terminal gnome-mines libreoffice-fresh firefox-i18n-es-es firefox-adblock-plus cups system-config-printer blueberry
+trizen --noconfirm -Sy thunar xdg-user-dirs gvfs gvfs-smb thunar-volman thunar-archive-plugin file-roller gnome-disk-utility mousepad vlc atril ristretto galculator xfce4-terminal gnome-mines libreoffice-fresh-es hunspell-es_es firefox-i18n-es-es firefox-adblock-plus cups system-config-printer blueberry
 sudo systemctl enable cups.service
 sudo systemctl enable bluetooth.service
 sudo cp -r xfce/.config /etc/skel
