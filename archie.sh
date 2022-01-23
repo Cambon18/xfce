c0632caf917da01496faae2ba4ffcf5cf9b3b7e9
CambonOS <82413153+CambonOS@users.noreply.github.com>
2022-01-23 12:38:09 +0100

    Update archie.sh

--- archie.sh
+++ archie.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-trizen --noconfirm -Sy lightdm lightdm-gtk-greeter light-locker numlockx materia-gtk-theme xfce4-whiskermenu-plugin xfce4-session xfce4-panel xfce4-power-manager xfce4-settings xfce4-screenshooter xfconf xfdesktop xfwm4 network-manager-applet xfce4-pulseaudio-plugin pulseaudio pulseaudio-jack pulseaudio-bluetooth pavucontrol mugshot arc-gtk-theme papirus-icon-theme && \
+trizen --noconfirm -Sy lightdm lightdm-gtk-greeter light-locker materia-gtk-theme xfce4-whiskermenu-plugin xfce4-session xfce4-panel xfce4-power-manager xfce4-settings xfce4-screenshooter xfconf xfdesktop xfwm4 network-manager-applet xfce4-pulseaudio-plugin pulseaudio pulseaudio-jack pulseaudio-bluetooth pavucontrol mugshot arc-gtk-theme papirus-icon-theme && \
 trizen --noconfirm -Sy thunar xdg-user-dirs gvfs gvfs-smb thunar-volman thunar-archive-plugin file-roller gnome-disk-utility mousepad vlc atril ristretto galculator xfce4-terminal libreoffice-fresh-es hunspell-es_es chromium cups system-config-printer blueberry && \
 sudo systemctl enable cups.service && \
 sudo systemctl enable bluetooth.service && \
