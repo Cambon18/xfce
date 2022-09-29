28e74c3363f6afbce3c542f52807e0e6240e10a1
Cambon18 <82413153+Cambon18@users.noreply.github.com>
2022-09-29 11:14:47 +0200

    Update archie.sh

--- archie.sh
+++ archie.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+yay --noconfirm -Sy $(cat list-packages)
 yay --noconfirm -Sy xorg-server lightdm lightdm-gtk-greeter light-locker materia-gtk-theme xfce4-whiskermenu-plugin xfce4-session xfce4-panel xfce4-power-manager xfce4-settings xfce4-screenshooter xfconf xfdesktop xfwm4 network-manager-applet xfce4-pulseaudio-plugin pulseaudio pulseaudio-jack pulseaudio-bluetooth pavucontrol mugshot arc-gtk-theme papirus-icon-theme && \
 yay --noconfirm -Sy thunar xdg-user-dirs gvfs gvfs-smb thunar-volman thunar-archive-plugin file-roller gnome-disk-utility mousepad vlc atril ristretto galculator xfce4-terminal libreoffice-fresh-es hunspell-es_es firefox-i18n-es-es cups system-config-printer blueberry && \
-sudo systemctl enable cups.service && \
-sudo systemctl enable bluetooth.service && \
-sudo cp -rv lightdm /etc && \
-sudo systemctl enable lightdm.service && \
+sudo systemctl enable bluetooth.service
+sudo systemctl enable cups.service
+sudo cp -rv lightdm /etc
+sudo systemctl enable lightdm.service
 sudo cp -rv skel /etc
