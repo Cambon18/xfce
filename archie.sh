52b7d98166b1628bb75d0db6821bae48e097917a
Cambon18 <82413153+Cambon18@users.noreply.github.com>
2022-10-02 15:15:38 +0200

    Update archie.sh

--- archie.sh
+++ archie.sh
@@ -5,4 +5,5 @@ sudo systemctl enable cups.service
 sudo cp -rv lightdm /etc
 sudo systemctl enable lightdm.service
 sudo cp -rv skel /etc
+sudo cp -rv slick-greeter /usr/share
 sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
